18d9667b31fa8ef885fb9a7d837515c54ca2a89e
Your Name <you@example.com>
2023-04-18 05:08:44 +0600

    adding logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Logo from '../assets/logo.png'
 function Header() {
   return (
-    <header className='bg-[#3a3a3a95] py-2.5 px-3 flex justify-around'>
-      <div id='logo'>samprism</div>
+    <header className='bg-gray-800 py-2.5 px-3 flex justify-around items-center'>
+      <div className='flex gap-3 items-center'>
+        <img src={Logo} className='w-36 h-auto' />
+        <a
+          href='https://wvsr.github.io'
+          className='text-lg text-gray-400 hover:text-gray-300'
+        >
+          by sami
+        </a>
+      </div>
       <div>Keyboard shortcut</div>
     </header>
   )
